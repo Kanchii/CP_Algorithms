bd2b18e5926ce60ec3abf1465493b26c03fad27d
Weiss <weisslipe@gmail.com>
2018-09-21 17:16:44 -0300

    MergeTree do capeta

--- Data_Structures/MergeTree.cpp
+++ Data_Structures/MergeTree.cpp
@@ -6,7 +6,7 @@ vector<int> mergeTree[1234];
 
 void createMergeTree(vector<int> &vet, int l, int r, int pos){
     if(l == r){
-        // cout << l << " " << r << ": " << vet[l] << endl;
+        cout << l << " " << r << ": " << vet[l] << endl;
         mergeTree[pos] = vector<int>(1, vet[l]);
     } else {
         int mid = (l + r) / 2;
@@ -15,37 +15,21 @@ void createMergeTree(vector<int> &vet, int l, int r, int pos){
         merge(mergeTree[2 * pos + 1].begin(), mergeTree[2 * pos + 1].end(),
               mergeTree[2 * pos + 2].begin(), mergeTree[2 * pos + 2].end(),
               back_inserter(mergeTree[pos]));
-        // cout << l << " " << r << ": ";
-        // for(int v : mergeTree[pos]) cout << v << " ";
-        // cout << endl;
-    }
-}
-
-int query(int l, int r, int ql, int qr, int pos, int kth){
-    if(l > qr or r < ql) return 1e9;
-    if(l == r) return mergeTree[pos][0];
-    int mid = (l + r) / 2;
-    if(mid < ql){
-        return query(mid + 1, r, ql, qr, 2 * pos + 2, kth);
-    }
-    int left = mid - ql + 1;
-    if(left >= kth){
-        return query(l, mid, ql, qr, 2 * pos + 1, kth);
-    } else {
-        return query(mid + 1, r, mid + 1, qr, 2 * pos + 2, kth - left);
+        cout << l << " " << r << ": ";
+        for(int v : mergeTree[pos]) cout << v << " ";
+        cout << endl;
     }
 }
 
 int main(int argc, char const *argv[]) {
 
     int n = 10;
-    int arr[] = {0, 2, 10, -5, 3, 1, 5, 0, 8, -3};
+    int arr[] = {0, 2, 10, -5, 3, 1, 5, 6, 8, -3};
+
+    // -5 1 2 3 5 10
     vector<int> vet(arr, arr + n);
     createMergeTree(vet, 0, n - 1, 0);
-
-    int kth = 3;
-    int q = query(0, n - 1, 1, 6, 0, kth);
-    cout << q << endl; // Saida: 2
+    
 
 
     return 0;
